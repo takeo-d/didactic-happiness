9585374dbbafaf184b2762b0503570f3e391180c
タケオD <donribetchio@gmail.com>
2024-08-27 15:44:57 +0900

    config,routesをインデックス

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\PostController; //
+Route::get('/posts', [PostController::class, 'index']);   
